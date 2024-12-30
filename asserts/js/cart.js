7e33d4f65b15789b82a38fab76535819efff1f4d
Sai Krishnan.s <saikrishnan.s2024cse@sece.ac.in>
2024-12-30 15:45:17 +0530

    feat: update file paths for CSS and JS; add clear cart functionality

---
+++ asserts/js/cart.js
@@ -0,0 +1,83 @@
+let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+function toggleShoppingCart() {
+    const cart = document.querySelector('.cart');
+    if (cart.style.display === 'block') {
+        cart.style.display = 'none';
+    } else {
+        cart.style.display = 'block';
+    }
+}
+
+function closeShoppingCart() {
+    document.querySelector('.cart').style.display = 'none';
+}
+
+function clearCart() {
+    cart = [];
+    localStorage.setItem('cart', JSON.stringify(cart));
+    renderCart();
+}
+function continueShopping(){
+    document.querySelector('.cart').style.display = 'none';
+}
+
+
+function addToCart(productName, productPrice, productQuantity) {
+    let total = document.querySelector('.total').textContent;
+    let quantity = document.querySelector('.quantity').textContent;
+    total = parseInt(total);
+    quantity = parseInt(quantity);
+    total += productPrice * productQuantity;
+    quantity += productQuantity;
+    document.querySelector('.total').textContent = total;
+    document.querySelector('.quantity').textContent = quantity;
+
+    let existingProduct = cart.find(item => item.name === productName);
+
+    if (existingProduct) {
+        existingProduct.quantity += productQuantity;
+    } else {
+        cart.push({ name: productName, price: productPrice, quantity: productQuantity });
+    }
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+    renderCart();
+    toggleShoppingCart();
+}
+function renderCart() {
+    let productList = document.querySelector('.listcard');
+    productList.innerHTML = '';
+    let total = 0;
+    let totalQuantity = 0;
+
+    cart.forEach(item => {
+        total += item.price * item.quantity;
+        totalQuantity += item.quantity;
+
+        let product = document.createElement('div');
+        product.classList.add(`product-${item.name.replace(/\s+/g, '-').toLowerCase()}`);
+        product.innerHTML = `<p>${item.name}</p><p>₹${item.price.toLocaleString()}</p><p>Quantity: <span class="product-quantity">${item.quantity}</span></p>`;
+        productList.appendChild(product);
+    });
+
+    document.querySelector('.total').textContent = total.toLocaleString();
+    document.querySelector('.quantity').textContent = totalQuantity;
+}
+function removeProduct(productName) {
+    let product = cart.find(item => item.name === productName);
+    let total = document.querySelector('.total').textContent;
+    let quantity = document.querySelector('.quantity').textContent;
+    total = parseInt(total);
+    quantity = parseInt(quantity);
+    total -= product.price * product.quantity;
+    quantity -= product.quantity;
+    document.querySelector('.total').textContent = total;
+    document.querySelector('.quantity').textContent = quantity;
+
+    cart = cart.filter(item => item.name !== productName);
+    localStorage.setItem('cart', JSON.stringify(cart));
+    renderCart();
+}
+
+document.addEventListener('DOMContentLoaded', renderCart);
--- asserts/js/shop.js
+++ asserts/js/shop.js
@@ -1,12 +1,12 @@
 let products = [
-    { id: 1, name: 'iphone 15 plus', image: 'Images/Iphone/iphone 15 plus.webp', price: 93900 },
-    { id: 2, name: 'iphone 15 pro max', image: 'Images/Iphone/iPhone_15_Pro_Max.webp', price: 179900 },
-    { id: 3, name: 'iphone 16 plus', image: 'Images/Iphone/iphone 16 plus.webp', price: 119900 },
-    { id: 4, name: 'iphone 16 pro max', image: 'Images/Iphone/iphone-16-pro max.webp', price: 184900 },
-    { id: 5, name: 'samsung galaxy s23', image: 'Images/samsung/Samsung-Galaxy-S23-FE.jpg', price: 74999 },
-    { id: 6, name: 'samsung galaxy s23 ultra', image: 'Images/samsung/samsung galaxy s23 ultra.webp', price: 89999 },
-    { id: 7, name: 'samsung galaxy s24', image: 'Images/samsung/samsung galaxy s24 FE.jfif', price: 74999 },
-    { id: 8, name: 'samsung galaxy s24 ultra', image: 'Images/samsung/samsung galaxy s24 ultra.jpg', price: 99999 },
+    { id: 1, name: 'Iphone 16 Pro Max', image: 'asserts/image/Iphone/iphone-16-pro max.webp', price: 184900 },
+    { id: 2, name: 'Iphone 16 Plus', image: 'asserts/image/Iphone/iphone 16 plus.webp', price: 119900 },
+    { id: 3, name: 'Iphone 15 Pro Max', image: 'asserts/image/Iphone/iPhone_15_Pro_Max.webp', price: 179900 },
+    { id: 4, name: 'Iphone 15 Plus', image: 'asserts/image/Iphone/iphone 15 plus.webp', price: 93900 },
+    { id: 5, name: 'Samsung Galaxy S24 Ultra', image: 'asserts/image/samsung/samsung galaxy s24 ultra.jpg', price: 99999 },
+    { id: 6, name: 'Samsung Galaxy S24 FE', image: 'asserts/image/samsung/samsung galaxy s24 FE.jfif', price: 74999 },
+    { id: 7, name: 'Samsung Galaxy S23 Ultra', image: 'asserts/image/samsung/samsung galaxy s23 ultra.webp', price: 89999 },
+    { id: 8, name: 'Samsung Galaxy S23 FE', image: 'asserts/image/samsung/Samsung-Galaxy-S23-FE.jpg', price: 74999 }, 
 ];
 let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
@@ -19,7 +19,8 @@ function initApp() {
             <img src="${product.image}" alt="${product.name}" />
             <p>${product.name}</p>
             <p>Price: ${product.price}</p>
-            <button onclick="addToCart(${index})">Add to Cart</button>
+            <button onclick="addToCart(${index})">Add to Cart<i class='bx bx-shopping-bag'></i></button>
+            <button onclick="buyNow(${index})">Buy Now<i class='bx bx-chevrons-right'></i></button>
         `;
         list.appendChild(newDiv);
     });
@@ -35,6 +36,7 @@ function addToCart(index) {
     }
     localStorage.setItem('cart', JSON.stringify(cart));
     alert(`${products[index].name} has been added to your cart.`);
+    openShoppingCart();
 }
 
 function openShoppingCart() {
@@ -55,7 +57,11 @@ function closeShoppingCart() {
 function continueShopping() {
     closeShoppingCart();
 }
-
+function clearCart() {
+    cart = [];
+    localStorage.setItem('cart', JSON.stringify(cart));
+    reloadCart();
+}
 function reloadCart() {
     let listcard = document.querySelector('.listcard');
     listcard.innerHTML = '';
@@ -100,5 +106,31 @@ cart[index].quantity = 0; // Reset the quantity to zero
 cart = cart.filter(item => item.quantity > 0); // Remove the item from the cart
 localStorage.setItem('cart', JSON.stringify(cart)); // Save the updated cart
 reloadCart(); // Refresh the cart display
+}
+function buyNow(index) {
+    if(products[index].name ==='Iphone 16 Pro Max'){
+        window.location.href ="iphone 16 pro max.html";
+    }
+    else if(products[index].name ==='Iphone 16 Plus'){
+        window.location.href ="iphone 16 plus.html";
+    }
+    else if(products[index].name ==='Iphone 15 Pro Max'){
+        window.location.href ="iphone 15 pro max.html";
+    }
+    else if(products[index].name ==='Iphone 15 Plus'){
+        window.location.href ="iphone 15 plus.html";
+    }
+    else if(products[index].name ==='Samsung Galaxy S24 Ultra'){
+        window.location.href ="samsung galaxy s24 ultra.html";
+    }
+    else if(products[index].name ==='Samsung Galaxy S24 FE'){
+        window.location.href ="samsung galaxy s24 FE.html";
+    }
+    else if(products[index].name ==='Samsung Galaxy S23 Ultra'){
+        window.location.href ="samsung galaxy s23 ultra.html";
+    }
+    else if(products[index].name ==='Samsung Galaxy S23 FE'){
+        window.location.href ="samsung galaxy s23 FE.html";
+    }
 }
 initApp();
